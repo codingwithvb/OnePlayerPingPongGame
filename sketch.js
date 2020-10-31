1072958710baee1849ff8d8cbf4688e2d461ce31
codingwithvb <72670340+codingwithvb@users.noreply.github.com>
2020-10-31 12:02:58 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,13 @@
 var userPaddle, computerPaddle, computerScore, playerScore, gameState, ball,scoreSound, wall_hitSound, hitSound;
 
-function preload(){
-  // scoreSound = loadSound('score.mp3');
-  // wall_hitSound = loadSound('wall_hit.mp3');
-  // hitSound = loadSound('hit.mp3');
-}
-
 function setup() {
   
 createCanvas(400,400);
-
-//create a user paddle sprite
+ 
 userPaddle = createSprite(390,200,10,70);
-
-//create a computer paddle sprite
+  
 computerPaddle = createSprite(10,200,10,70);
-
-//create the pong ball
+  
 ball = createSprite(200,200,12,12);
 
 computerScore = 0;
@@ -25,14 +16,13 @@ gameState = "serve";
 }
 
 function draw() {  
-  //fill the computer screen with white color
+ 
   background("white");
   edges = createEdgeSprites();
-  //display Scores
+
   text(computerScore,170,20);
   text(playerScore, 230,20);
 
-  //draw dotted lines
   for (var i = 0; i < 400; i+=20) {
      line(200,i,200,i+10);
   }
@@ -51,38 +41,27 @@ function draw() {
     computerScore = 0;
     playerScore = 0;
   }
-
-
-  //give velocity to the ball when the user presses play
-  //assign random velocities later for fun
+  
   if (keyDown("space") && gameState == "serve") {
     ball.velocityX = 5;
     ball.velocityY = 5;
     gameState = "play";
   }
 
-  //make the userPaddle move with the mouse
+
   userPaddle.y = World.mouseY;
 
-
-
-  //make the ball bounce off the user paddle
   if(ball.isTouching(userPaddle)){
-    //hitSound.play();
     ball.x = ball.x - 5;
     ball.velocityX = -ball.velocityX;
   }
 
-  //make the ball bounce off the computer paddle
   if(ball.isTouching(computerPaddle)){
-    //hitSound.play();
     ball.x = ball.x + 5;
     ball.velocityX = -ball.velocityX;
   }
-
-  //place the ball back in the centre if it crosses the screen
+  
   if(ball.x > 400 || ball.x < 0){
-    //scoreSound.play();
 
   if (ball.x < 0) {
       playerScore++;
@@ -102,14 +81,10 @@ function draw() {
     }
   }
 
-  //make the ball bounce off the top and bottom walls
   if (ball.isTouching(edges[2]) || ball.isTouching(edges[3])) {
     ball.bounceOff(edges[2]);
     ball.bounceOff(edges[3]);
-   // wall_hitSound.play();
   }
-
-  //add AI to the computer paddle so that it always hits the ball
   computerPaddle.y = ball.y;
   drawSprites();
 }
